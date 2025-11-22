e1bc95fbd898adbe2976928f73e450eb32cf683e
Johan Manuel Vicente B <jomavi582@gmail.com>
2025-11-21 20:15:36 -0400

    Creating DBContext

---
+++ HotelParadise_P3/HotelParadise_P3/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelParadise_P3.Entities
+{
+    [Table("Employees")]
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Position { get; set; }
+
+        public string? Phone { get; set; }
+
+        public DateTime Date_Admission { get; set; }
+    }
+
+}
